6b456299e955178387b95d582ea062cc3021c981
Sujit Jha <sujitjha@Sujit-Jha-Hakkoda-JQGL4GC2MX.local>
2025-09-19 10:36:28 +0530

    removed subfolder

---
+++ target/run/dbt_fivtran_snowflake/models/demo/concat.sql
@@ -0,0 +1,16 @@
+
+  create or replace   view FIVETRAN_DATABASE.PUBLIC.concat
+  
+  
+  
+  
+  as (
+    with concat_data as (
+    select * from FIVETRAN_DATABASE.GOOGLE_SHEETS.GOOGLE_SHEETS_DEMO_DB
+),
+final as (
+    select SIGNUP_DATE, GENDER, PHONE, LOYALTY_TIER, CUSTOMER_SK, CUSTOMER_CODE, CONCAT(FIRST_NAME,' ',LAST_NAME) AS FULL_NAME, EMAIL from concat_data
+)
+select * from final
+  );
+
